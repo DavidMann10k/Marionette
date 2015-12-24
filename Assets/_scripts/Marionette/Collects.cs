e751e4ba63ba5adfc4f8b7b65311cf4aaa6f8efc
DavidMann10k <davidmann10k@gmail.com>
2015-12-23 21:58:50 -0600

    stub out collects behavior

---
+++ Assets/_scripts/Marionette/Collects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Marionette
+{
+	public class Collects : MonoBehaviour, IBehavior
+	{
+		
+
+		public void ConcludeDirective (IDirective directive)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
