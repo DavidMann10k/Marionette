6b6a57f8f9ad3f734910505a148b139e67b7478e
DavidMann10k <davidmann10k@gmail.com>
2015-12-30 21:17:29 -0600

    stub out collects, senses

--- Assets/_scripts/Marionette/Collects.cs
+++ Assets/_scripts/Marionette/Collects.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Marionette
 {
 	// Adds collecting behavior to an AI agent
+	[RequireComponent (typeof(Marionette))]
+	[RequireComponent (typeof(Senses))]
 	public class Collects : MonoBehaviour, IBehavior
 	{
-		
+		[SerializeField]
+		List<string> item_names;
+
+		Marionette marionette;
+		Senses senses;
 
 		public void ConcludeDirective (IDirective directive)
 		{
 			throw new NotImplementedException ();
 		}
+
+		void Awake ()
+		{
+			marionette = GetComponent<Marionette> ();
+			senses = GetComponent<Senses> ();
+		}
+
+		void Start ()
+		{
+			// if there are nearby objects of interest
+			//   if objects of interest are withing range
+			//     create store directive
+			//   else
+			//     create move directive
+			//   else
+			//     do nothing
+		}
 	}
 }
-
---
+++ Assets/_scripts/Marionette/Senses.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Marionette
+{
+	// a collection of tools used to mimic senses such as sight or smell
+	[RequireComponent (typeof(Marionette))]
+	public class Senses : MonoBehaviour
+	{
+		
+	}
+}
