81faaf6dfcae69731380d4717c61f26686c722ba
DavidMann10k <davidmann10k@gmail.com>
2016-01-10 22:56:41 -0600

    prototype Living

---
+++ Assets/_scripts/Marionette/Living.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Marionette
+{
+	public class Living : MonoBehaviour
+	{
+		public int Life { 
+			get { return life; }
+		}
+
+		[SerializeField]
+		int life = 5;
+
+		public ParticleSystem particle;
+
+		public void OnDamage (int damage)
+		{
+			life -= damage;
+			if (life <= 0)
+				OnDeath ();
+		}
+
+		public void OnDeath ()
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/_scripts/TestLiving.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Marionette
+{
+	public class TestLiving : MonoBehaviour
+	{
+
+		public ParticleSystem particle;
+		GameObject selected;
+
+		string message = "OnDamage";
+
+		// Use this for initialization
+		void Start ()
+		{
+			Click.SelectgGameObject += new EventHandler<SelectGameObjectArgs> (OnSelectGameObject);
+		}
+
+		void OnSelectGameObject (object sender, SelectGameObjectArgs e)
+		{
+			selected = e.GameObject;
+		}
+
+		void runParticle ()
+		{
+			particle.Clear ();
+			particle.transform.position = selected.transform.position;
+			particle.Play ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.GetMouseButtonUp (1)) {
+				selected.SendMessage (message, 1);
+				runParticle ();
+			}
+		}
+	}
+}
