fb664d855013d55b2fafc5a7f971c69a90d29805
David Mann <david@mann.engineer>
2018-01-24 10:59:25 -0800

    Move test scripts to Test namespace.

---
+++ Assets/_scripts/Test/TestParticle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Test
+{
+	public class TestParticle : MonoBehaviour {
+
+		public ParticleSystem particle;
+
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetMouseButtonUp(0))
+			{
+				particle.Clear();
+				particle.Play();
+
+				var em = particle.emission;
+				em.enabled = true;
+			}
+		}
+	}
+}
---
+++ Assets/_scripts/Test/TestRtree.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTree;
+
+namespace Test
+{
+	public class TestRtree : MonoBehaviour
+	{
+		RTree.RTree<GameObject> tree = new RTree.RTree<GameObject> ();
+
+		void Start ()
+		{
+
+		}
+	}
+}
