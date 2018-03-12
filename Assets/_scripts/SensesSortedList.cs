9146fe02174b7e58d93aa3f8d232ad56b1a3cd3c
David Mann <david@mann.engineer>
2018-03-11 23:45:02 -0500

    add sorted list senses variant

---
+++ Assets/_scripts/SensesSortedList.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SensesSortedList : MonoBehaviour {
+
+	public float SenseRange = 10f;
+
+	static List<IndexItem> senses_list = new List<IndexItem> ();
+	List<SensesSortedList> nearby_senses_cache = new List<SensesSortedList>();
+
+//	Vector3 indexed_position;
+
+	IndexItem index_item;
+
+	void Start() {
+		index_item = new IndexItem (transform.position, this);
+		senses_list.Add (index_item);
+	}
+
+	void Update() {
+		
+		CacheNearbySenses ();
+		IndexPosition ();
+	}
+
+	void IndexPosition() {
+		index_item.Position = transform.position;
+
+//		if ((transform.position - indexed_position).sqrMagnitude > .1f)
+//		{
+//			indexed_position = transform.position;
+//
+//		}
+	}
+
+	[Obsolete]
+	void CacheNearbySensesLinq()
+	{
+		nearby_senses_cache.Clear ();
+
+		nearby_senses_cache = senses_list.Where( i => 
+			i.Position.x < transform.position.x + SenseRange &&
+			i.Position.x > transform.position.x - SenseRange &&
+			i.Position.z < transform.position.z + SenseRange &&
+			i.Position.z > transform.position.z - SenseRange).Select(j => j.Object).ToList();
+		//		foreach (IndexItem i in senses_list) {
+		//			if ((i.Object.transform.position - transform.position).sqrMagnitude < Range * Range)
+		//				nearby_senses_cache.Add (i.Object);
+		//		}
+	}
+
+	bool IsInRange(Vector3 point)
+	{
+		return (point - transform.position).sqrMagnitude < SenseRange * SenseRange;
+	}
+
+	void CacheNearbySenses()
+	{
+		nearby_senses_cache.Clear ();
+		
+		foreach (IndexItem i in senses_list) {
+			if ((i.Object.transform.position - transform.position).sqrMagnitude < SenseRange * SenseRange)
+				nearby_senses_cache.Add (i.Object);
+		}
+	}
+
+	void OnDrawGizmos() {
+		foreach (SensesSortedList s in nearby_senses_cache) {
+			Gizmos.DrawLine (transform.position, s.transform.position);
+		}
+	}
+
+	void OnDestroy()
+	{
+		senses_list.Remove (index_item);
+	}
+
+	struct IndexItem {
+		public SensesSortedList Object  { get; private set; }
+		public Vector3 Position { get; set; }
+
+		public IndexItem (Vector3 position, SensesSortedList game_object)
+			: this()
+		{
+			Object = game_object;
+			Position = position;
+		}
+	}
+}
+
