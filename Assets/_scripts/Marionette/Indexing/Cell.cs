46a62ddeae0033306ee656eba5e17a34a749f817
David Mann <david@mann.engineer>
2018-03-19 20:20:42 -0500

    add cell item linked list

--- Assets/_scripts/Marionette/Indexing/Cell.cs
+++ Assets/_scripts/Marionette/Indexing/Cell.cs
@@ -1,6 +1,20 @@
-﻿namespace Marionette.Indexing {
-	public struct Cell<T> {
-		
+﻿using System.Collections.Generic;
+
+namespace Marionette.Indexing {
+	public class Cell<T> {
+		LinkedList<T> items;
+
+		public Cell() {
+			items = new LinkedList<T> ();
+		}
+
+		public void Add(T item) {
+			items.AddFirst(item);
+		}
+
+		public void Remove(T item) {
+			items.Remove(item);
+		}
 	}
 }
 
